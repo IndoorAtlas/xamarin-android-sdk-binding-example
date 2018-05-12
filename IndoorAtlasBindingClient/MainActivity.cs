58f02454b0c0e8ae6bb525a8ee538405b7b873c1
Jukka Raanamo <jukka.raanamo@gmail.com>
2018-05-12 07:23:07 +0300

    Example project related to knowledge base article

---
+++ IndoorAtlasBindingClient/MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Indooratlas.Android.Sdk;
+
+namespace IndoorAtlasBindingClient
+{
+    [Activity(Label = "IndoorAtlasBindingClient", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity, IALocationListener
+    {
+        IALocationManager locationManager;
+		TextView textViewStatus;
+		TextView textViewLocation;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			textViewStatus = FindViewById(Resource.Id.textViewStatus) as TextView;
+			textViewLocation = FindViewById(Resource.Id.textViewLocation) as TextView;
+
+			locationManager = IALocationManager.Create(this);
+
+           
+        }
+
+
+		protected override void OnResume()
+        {
+            base.OnResume();
+            locationManager.RequestLocationUpdates(IALocationRequest.Create(),
+                                                   this);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            locationManager.RemoveLocationUpdates(this);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            locationManager.Destroy();
+        }
+
+		public void OnLocationChanged(IALocation location)
+        {
+			textViewLocation.Text = location.ToString();
+        }
+
+        public void OnStatusChanged(string provider, int status, Bundle extras)
+        {
+			textViewStatus.Text = provider + ":" + status + ", " + extras;
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # xamarin-android-sdk-binding-example
 Xamarin Binding for IndoorAtlas Android SDK
+
+Read related documentation from: https://indooratlas.freshdesk.com/support/solutions/articles/36000058576-how-to-use-indooratlas-android-sdk-with-xamarin
